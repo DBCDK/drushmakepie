39e5911713c167fa3ae874ad5252cca443d52d0d
Michael Carlsson <mca@dbc.dk>
2013-01-09 14:33:08 +0100

    Added support for version in long format.

--- drushmake.py
+++ drushmake.py
@@ -1,6 +1,6 @@
 from pyparsing import *
 
-
+# Symbols and basic tokens
 LBRCKT = Literal('[').suppress()
 RBRCKT = Literal(']').suppress()
 EQUAL = Literal('=').suppress()
@@ -8,15 +8,26 @@ HASH = oneOf('# ;').suppress()
 API = Keyword('api').suppress()
 CORE = Keyword('core').suppress()
 PROJECTS = Keyword('projects').suppress()
+VERSION = Keyword('version').suppress()
+
+# Comment
 optional_text = Combine(Optional(Word(printables + ' ')))
 comment = HASH + optional_text('comment')
+
+# Api
 api_no = Word(nums) | Word(nums)
 api = API + EQUAL + api_no('api')
+
+# Core
 major_x = Combine(Word(nums) + Literal('.x'))
 core = CORE + EQUAL + major_x('core')
+
+# Projects
 name = Word(alphas, alphanums + '_')('name')
-version = Combine(Word(nums) + Literal('.') + Word(nums))('version')
-projects = (PROJECTS + LBRCKT + name + RBRCKT + EQUAL + version)('projects')
+short_version = EQUAL + Combine(Word(nums) + Literal('.') + Word(nums))('version')
+long_version = LBRCKT + VERSION + RBRCKT + short_version
+version = short_version | long_version
+projects = (PROJECTS + LBRCKT + name + RBRCKT + version)('projects')
 
 
 makefile_grammar = comment | ((api | core | projects) + Optional(comment))
