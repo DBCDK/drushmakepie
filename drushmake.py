42073a50c3b8e6963a83d37633a9e60f7d2e80cd
Michael Carlsson <mca@dbc.dk>
2013-01-09 09:13:12 +0100

    Parsing of comments, api, core, and projects just specifying version.

---
+++ drushmake.py
@@ -0,0 +1,32 @@
+from pyparsing import *
+
+
+LBRCKT = Literal('[').suppress()
+RBRCKT = Literal(']').suppress()
+EQUAL = Literal('=').suppress()
+HASH = Literal('#').suppress()
+API = Keyword('api').suppress()
+CORE = Keyword('core').suppress()
+PROJECTS = Keyword('projects').suppress()
+optional_text = ZeroOrMore(Word(printables)).setParseAction(lambda tokens: ' '.join(tokens))
+comment = (HASH + optional_text)('comment')
+api_no = Word(nums) | Word(nums)
+api = API + EQUAL + api_no('api')
+major_x = Combine(Word(nums) + Literal('.x'))
+core = (Suppress(Literal('core')) + EQUAL + major_x)('core')
+name = Word(alphas, alphanums + '_')('name')
+version = Combine(Word(nums) + Literal('.') + Word(nums))('version')
+projects = (PROJECTS + LBRCKT + name + RBRCKT + EQUAL + version)('projects')
+
+
+makefile_grammar = comment | ((api | core | projects) + Optional(comment))
+
+def parseline(line):
+  def collapsespaces(chars):
+    return ' '.join(chars.split())
+
+  if line == '':
+    return ()
+  else:
+    tokens = makefile_grammar.parseString(collapsespaces(line))
+    return tokens
