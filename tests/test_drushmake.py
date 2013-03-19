e885ff80807b8209d3bcd3500ab40731ac2c22e0
Michael Carlsson <mca@dbc.dk>
2013-03-19 14:01:04 +0100

    Removed use of unittest framework and simplified asserts.

--- tests/test_drushmake.py
+++ tests/test_drushmake.py
@@ -1,7 +1,7 @@
-from unittest import TestCase
 import drushmake
 
-class TestDrushmakeParsefile(TestCase):
+
+class TestDrushmakeParsefile:
   def prepareInput(self, file):
     """
     Helper function to parse the single lines.
@@ -14,14 +14,13 @@ class TestDrushmakeParsefile(TestCase):
     return z
 
   def testComments(self):
-    """Test various comments."""
     expected = {'drupal': [], 'libraries': [], 'modules': [], 'themes': []}
     simple_comment = self.prepareInput(['# test'])
     no_comment = self.prepareInput(['api = 2', 'core = 7.x'])
     api_core_comment = self.prepareInput(['api = 2', 'core = 7.x', '#test'])
-    self.assertEqual(drushmake.parsefile(simple_comment), expected, 'Simple comment.')
-    self.assertEqual(drushmake.parsefile(no_comment), expected, 'Only api and core specified.')
-    self.assertEqual(drushmake.parsefile(api_core_comment), expected, 'Api, core, and a comment.')
+    assert drushmake.parsefile(simple_comment) == expected
+    assert drushmake.parsefile(no_comment) == expected
+    assert drushmake.parsefile(api_core_comment) == expected
 
   def testDrupal(self):
     expected = {'drupal': [{'version': '7.17'}],
@@ -29,4 +28,4 @@ class TestDrushmakeParsefile(TestCase):
                 'modules': [],
                 'themes': []}
     drupal_version_only = self.prepareInput(['api = 2', 'core = 7.x', 'projects[drupal] = 7.17'])
-    self.assertEqual(drushmake.parsefile(drupal_version_only), expected, 'Drupal version.')
+    assert drushmake.parsefile(drupal_version_only) == expected
