b303159d18d7304a7ed5e44f47df64cad8d25057
Michael Carlsson <mca@dbc.dk>
2013-04-02 14:30:50 +0200

    Rewrote doctest for parsing download type to python code.

--- tests/test_parseline_libraries.py
+++ tests/test_parseline_libraries.py
@@ -32,3 +32,33 @@ class TestParselineLibrariesUrl:
         line = 'libraries[my_library][download][url] = http://oss.dbc.dk/public space/fake_library.tar.gz'
         with pytest.raises(drushmake.ParseException):
             drushmake.parseline(line)
+
+class TestParselineLibrariesType:
+    def testGit(self):
+        line = 'libraries[my_library][download][type] = git'
+        result = drushmake.parseline(line)
+        assert result['libraries']['name'] == 'my_library'
+        assert result['libraries']['download']['type'] == 'git'
+
+    def testFile(self):
+        line = 'libraries[my_library][download][type] = file'
+        result = drushmake.parseline(line)
+        assert result['libraries']['name'] == 'my_library'
+        assert result['libraries']['download']['type'] == 'file'
+
+    def testSvn(self):
+        line = 'libraries[my_library][download][type] = svn'
+        result = drushmake.parseline(line)
+        assert result['libraries']['name'] == 'my_library'
+        assert result['libraries']['download']['type'] == 'svn'
+
+    def testBzr(self):
+        line = 'libraries[my_library][download][type] = bzr'
+        result = drushmake.parseline(line)
+        assert result['libraries']['name'] == 'my_library'
+        assert result['libraries']['download']['type'] == 'bzr'
+
+    def testXyz(self):
+        line = 'libraries[my_library][download][type] = xyz'
+        with pytest.raises(drushmake.ParseException):
+            drushmake.parseline(line)
