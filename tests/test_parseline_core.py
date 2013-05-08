673245b4b47cc9e6e50d7e9927a01ddd16ddcb5f
Michael Carlsson <mca@dbc.dk>
2013-05-08 14:44:19 +0200

    Core tests made specific for testing core statement.

--- tests/test_parseline_core.py
+++ tests/test_parseline_core.py
@@ -1,15 +1,16 @@
 from __future__ import with_statement
+from makegrammar import core, ParseException
 import pytest
-import drushmake
 
 
-def testParselineCore():
-    assert drushmake.parseline('core = 7.x')['core'] == '7.x'
-    with pytest.raises(drushmake.ParseException):
-        drushmake.parseline('core = 7 .x')['core']
-    with pytest.raises(drushmake.ParseException):
-        drushmake.parseline('core = 7. x')['core']
-    assert drushmake.parseline("""core
-        = 7.x""")['core'] == '7.x'
-    assert drushmake.parseline("""core =
-        7.x""")['core'] == '7.x'
+def testCore():
+    assert core.parseString('core=6.x')['core'] == '6.x'
+    assert core.parseString('core = 7.x')['core'] == '7.x'
+    assert core.parseString("""core\n= 7.x""")['core'] == '7.x'
+    assert core.parseString("""core =\n7.x""")['core'] == '7.x'
+
+def testNotCore():
+    with pytest.raises(ParseException):
+        core.parseString('core = 7 .x')
+    with pytest.raises(ParseException):
+        core.parseString('core = 7. x')
