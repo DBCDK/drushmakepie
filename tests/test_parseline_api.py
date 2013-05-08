cc9a729ffc419a6feec0abbcc5fb729625bca92d
Michael Carlsson <mca@dbc.dk>
2013-05-08 14:33:44 +0200

    Api tests made specific for testing api statement.

--- tests/test_parseline_api.py
+++ tests/test_parseline_api.py
@@ -1,9 +1,16 @@
-import drushmake
+from __future__ import with_statement # Keyword 'with' not part of python 2.5
+from makegrammar import api, ParseException
+import pytest
 
 
 def testApi():
-    assert drushmake.parseline('api = 2')['api'] == '2'
-    assert drushmake.parseline('api=3')['api'] == '3'
-    result = drushmake.parseline('api = 1# comment')
-    assert (result['api'], result['comment']) == ('1', 'comment')
+    assert api.parseString('api = 2')['api'] == '2'
+    assert api.parseString('api=3')['api'] == '3'
+    assert api.parseString('api\n=\n4')['api'] == '4'
+
+def testNotApi():
+    with pytest.raises(ParseException):
+        api.parseString('api = ')
+    with pytest.raises(ParseException):
+        api.parseString('api')
 
