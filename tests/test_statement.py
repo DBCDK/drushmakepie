a6f5c21e5c870b4c8700e35285efa4b02e53980e
Michael Carlsson <mca@dbc.dk>
2013-05-22 13:01:16 +0200

    Statement tests made specific for testing statement.

--- tests/test_statement.py
+++ tests/test_statement.py
@@ -1,24 +1,32 @@
 from __future__ import with_statement
+from makegrammar import statement, ParseException
 import pytest
-import drushmake
 
 
 def testSplitLine():
-    result = drushmake.parseline("""projects[drupal][version]
+    result = statement.parseString("""projects[drupal][version]
         = 7.17""")
     assert result['projects']['name'] == 'drupal'
-    assert result['projects']['version'] == '7.17'
+    assert result['projects']['version_number'] == '7.17'
 
 def testWrongSplitLine1():
-    with pytest.raises(drushmake.ParseException):
-        drushmake.parseline("""projects
+    with pytest.raises(ParseException):
+        statement.parseString("""projects
             [drupal][version] = 7.17""")
 
 def testWrongSplitLine2():
-    with pytest.raises(drushmake.ParseException):
-        drushmake.parseline("""projects[drupal]
+    with pytest.raises(ParseException):
+        statement.parseString("""projects[drupal]
             [version] = 7.17""")
 
 def testSpaceInBrackets():
-    with pytest.raises(drushmake.ParseException):
-        drushmake.parseline("""projects[ drupal][version] = 7.17""")
+    with pytest.raises(ParseException):
+        statement.parseString("""projects[ drupal][version] = 7.17""")
+
+def testMatchComment():
+    result = statement.parseString(""" # this is a comment""")
+    assert result['comment'] == 'this is a comment'
+
+def testMatchStatementWithComment():
+    result = statement.parseString(""" api = 2 # this is a comment""")
+    assert result['comment'] == 'this is a comment'
